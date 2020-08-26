b5d9d84cb4044aae1131803edc5491e60687216b
Lakshmi Gurram <28781938+lakshmilux@users.noreply.github.com>
2020-08-26 22:51:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -90,7 +90,7 @@ def trainRouteClient():
 
 if __name__ == "__main__":
     #app.run(host = '0.0.0.0', port=8080)
-    app.run(debug = True,port = '5000',host = '0.0.0.0')
+    app.run(debug = True,port = '5000')
 
 
 
